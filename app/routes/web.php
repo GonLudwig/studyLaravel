bca02efa9ebc9bbccbbde976e23817c59b0ca4eb
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-01-13 15:44:58 -0300

    view class

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\Rota1Controller;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,17 +21,26 @@ use Illuminate\Support\Facades\Route;
 // })->where('sobrenome_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
-
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
-
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
-
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function () {
     Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
-    
     Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
-    
     Route::get('/produto', function(){ return 'Produtos'; })->name('app.produtos');
 });
+
+Route::get('/rota1/{p1}/{p2}', [Rota1Controller::class, 'teste'])->name('site.rota1');
+
+// Route::get('/rota2', function() {
+//     echo 'Rota2';
+//     return redirect()->route('site.rota1');
+// })->name('site.rota2');
+
+// Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function(){
+    echo 'Rota acessada não existe!<a href="'.route('site.index').'">Cliquei aqui</a> para ir a pagina inicial';
+});
+
